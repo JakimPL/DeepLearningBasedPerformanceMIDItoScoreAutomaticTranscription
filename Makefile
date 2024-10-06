81bc5e3063139ce86923935bd05f6c74b1afd137
JakimPL <jakimpl@gmail.com>
2024-10-06 14:21:26 +0200

    Updated: bibliography script

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 build:
+	python bibparser.py
 	lualatex -interaction=nonstopmode -shell-escape main.tex
 	bibtex main.aux
--- bibparser.py
+++ bibparser.py
@@ -2,16 +2,19 @@ from pathlib import Path
 import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
 
-PATH = Path("biblio.bib")
-TARGET_PATH = Path("bibliography.bib")
+BASE_PATH = Path("bibliography")
+PATH = BASE_PATH / "bibsource.bib"
+TARGET_PATH = BASE_PATH / "bibliography.bib"
 
 if __name__ == "__main__":
     with open(PATH, "r") as file:
         bibliography = bibtexparser.load(file)
 
     writer = BibTexWriter()
-    writer.indent = '    '
+    writer.indent = "    "
     writer.comma_first = False
 
     with open(TARGET_PATH, "w") as file:
         bibliography = file.write(writer.write(bibliography))
+
+    print("Bibliography updated.")
